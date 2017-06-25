e28b7928d7d164e96a2897c23529228d8c13f1eb
srallaba <srallaba@myune.edu.au>
2017-06-25 15:16:35 +1000

    Application

---
+++ application/controllers/Shed_52.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+// use namespace
+use Restserver\Libraries\REST_Controller;
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+
+class Shed_52 extends REST_Controller{
+      function __construct($config = 'rest'){
+            parent::__construct($config);
+   $this->load->database(); // load database
+   $this->load->model('Chook_model'); // load model
+}
+    
+    
+    public function index_get() {
+        
+       {
+        $this->load->helper('url');
+
+        $this->load->view('shed_52/index_52');
+    }
+}
+ 
+ 
+    }
+
+
+     
+
+   
+       
---
+++ application/controllers/api/Api_52.php
@@ -0,0 +1,140 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ use Restserver\Libraries\REST_Controller;
+require(APPPATH.'/libraries/REST_Controller.php');
+class Api_52 extends REST_Controller
+{
+ 
+       public function __construct() {
+               parent::__construct();
+               $this->load->model('Chook_model');
+ 
+       } 
+    
+    
+       public function antenna_report_get(){
+           
+           
+           
+$date = $this->input->get('date');
+if ($date != "") {
+$result = $this->Chook_model->show_antenna_data_by_date_52($date);
+
+if ($result != false) {
+$data['result_display'] = $result;
+} else {
+$data['result_display'] = "No record found !";
+}
+} else {
+    $data['date_error_message'] = "Date field is required";
+}    
+        
+          
+           
+           $this->response($data); 
+       }
+    
+       public function antenna_report_post(){
+           
+           
+           
+$date = $this->input->post('date');
+if ($date != "") {
+$result = $this->Chook_model->show_antenna_data_by_date_52($date);
+
+if ($result != false) {
+$data['result_display'] = $result;
+} else {
+$data['result_display'] = "No record found !";
+}
+} else {
+    $data['date_error_message'] = "Date field is required";
+}    
+        
+          
+           
+           $this->response($data); 
+       }
+    public function reader_report_get(){
+       $date = $this->input->get('date');
+        
+        
+        
+    //$result = $this->Chook_model->show_reader_data_by_date($date);
+        
+        if ($date != "") {
+ $result = $this->Chook_model->show_reader_data_by_date_52($date);
+
+if ($result != false) {
+$data['result_display'] = $result;
+} else {
+$data['result_display'] = "No record found !";
+}
+} else {
+    $data['date_error_message'] = "Date field is required";
+}    
+        
+       
+           $this->response($data); 
+       }
+    
+    
+     public function reader_report_post(){
+       $date = $this->input->post('date'); 
+         if ($date != "") {
+ $result = $this->Chook_model->show_reader_data_by_date_52($date);
+
+if ($result != false) {
+$data['result_display'] = $result;
+} else {
+$data['result_display'] = "No record found !";
+}
+} else {
+    $data['date_error_message'] = "Date field is required";
+}    
+        
+       
+           $this->response($data); 
+       }
+    
+    public function epc_report_get() {
+$result = $this->Chook_model->show_epc_data_52();
+if ($result != false) {
+$data['result_display'] = $result;
+} else {
+return 'Database is empty !';
+}
+//$data['show_table'] = $this->view_table();
+//$this->load->view('shed_53/index', $data);
+        $this->response($data); 
+}
+     
+    public function epc_report_post() {
+$result = $this->Chook_model->show_epc_data_52();
+if ($result != false) {
+$data['result_display'] = $result;
+} else {
+return 'Database is empty !';
+}
+//$data['show_table'] = $this->view_table();
+        $this->response($data); 
+//$this->load->view('shed_53/index', $data);
+}
+    
+   // public function epc_get(){
+          // $r = $this->Chook_model->show_epc_data();
+          // $this->response($r); 
+      // }
+      /*
+       public function user_post(){
+           $data = array('antenna_name' => $this->input->post('antenna_name'),
+           'reader_name' => $this->input->post('reader_name'),
+           'epc' => $this->input->post('epc')
+           );
+           $r = $this->user_model->insert($data);
+           $this->response($r); 
+       }
+       */
+      
+ 
+}
